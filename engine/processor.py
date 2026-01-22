3f00665f0bfb0ca283d0d8c5f4a619d534d12d21
NarbasHikmet <108670827+NarbasHikmet@users.noreply.github.com>
2026-01-22 23:02:50 +0300

    Update processor.py

--- engine/processor.py
+++ engine/processor.py
@@ -1,38 +1,11 @@
-import pandas as pd
-
 class HPProcessor:
-    """
-    HP Motor - 6 Fazlı Segmentasyon ve Katman İşlemcisi
-    """
-    def __init__(self):
-        self.phases = {
-            "F1": "Organized Defense",
-            "F2": "Defensive Transition",
-            "F3": "Organized Attack",
-            "F4": "Offensive Transition",
-            "F5": "Set Pieces (Att)",
-            "F6": "Set Pieces (Def)"
-        }
-
-    def segment_phases(self, df):
-        """
-        Ham veriyi zaman damgası ve aksiyon tipine göre 6 faza ayırır.
-        """
-        # Örnek kural bazlı segmentasyon (SportsBase 'action' kolonuna göre)
-        df['phase_hp'] = "UNKNOWN"
-        
-        # Basit kural seti:
+    def apply_lens(self, df):
+        # Zihin haritandaki kural setlerini uygular
+        df['phase_hp'] = "F0: Neutral"
         if 'action' in df.columns:
-            df.loc[df['action'].str.contains('corner|free kick', na=False), 'phase_hp'] = "F5"
-            df.loc[df['action'].str.contains('tackle|interception', na=False), 'phase_hp'] = "F1"
-            df.loc[df['action'].str.contains('shot|cross', na=False), 'phase_hp'] = "F3"
-            
-        return df
-
-    def apply_layers(self, df):
-        """
-        Veriyi Mikro (Bireysel), Mezzo (Grup), Makro (Takım) olarak etiketler.
-        """
-        df['layer_hp'] = "micro" # Varsayılan: Bireysel aksiyon
-        # Agregasyon varsa mezzo/macro etiketleri buraya gelir
+            df.loc[df['action'].str.contains('pass|build', na=False), 'phase_hp'] = "F3: Build-Up"
+            df.loc[df['action'].str.contains('tackle|interception', na=False), 'phase_hp'] = "F1: Org-Defense"
+            df.loc[df['action'].str.contains('corner|free kick', na=False), 'phase_hp'] = "F6: Set-Pieces"
+        
+        df['layer_hp'] = "micro" # Varsayılan bireysel eylem
         return df
